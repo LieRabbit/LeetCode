de12554bc8bd616c66a80ec80c13257b06d0d8c1
LieRabbit <lierabbitxyy@gmail.com>
2018-04-30 17:00:42 +0800

    添加注释，修改变量名

--- src/两个排序数组的中位数.java
+++ src/两个排序数组的中位数.java
@@ -15,20 +15,20 @@ public class 两个排序数组的中位数 {
         int n1 = nums1.length;
         int n2 = nums2.length;
         if (n1 < n2)
-            return findMedianSortedArrays(nums2, nums1);
-        int lo = 0, hi = n2 * 2;
+            return findMedianSortedArrays(nums2, nums1); // 确保nums2为短数组
+        int lo = 0, hi = n2 * 2; //
         while (lo <= hi) {
-            int mid2 = (lo + hi) >> 1;
-            int mid1 = n1 + n2 - mid2;
-            double L1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[(mid1 - 1) / 2];
-            double L2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[(mid2 - 1) / 2];
-            double R1 = (mid1 == n1 * 2) ? Integer.MAX_VALUE : nums1[mid1 / 2];
-            double R2 = (mid2 == n2 * 2) ? Integer.MAX_VALUE : nums2[mid2 / 2];
+            int c2 = (lo + hi) >> 1;
+            int c1 = n1 + n2 - c2;
+            double L1 = (c1 == 0) ? Integer.MIN_VALUE : nums1[(c1 - 1) / 2];
+            double L2 = (c2 == 0) ? Integer.MIN_VALUE : nums2[(c2 - 1) / 2];
+            double R1 = (c1 == n1 * 2) ? Integer.MAX_VALUE : nums1[c1 / 2];
+            double R2 = (c2 == n2 * 2) ? Integer.MAX_VALUE : nums2[c2 / 2];
 
             if (L1 > R2)
-                lo = mid2 + 1;
+                lo = c2 + 1; // 增大c1，减小c2
             else if (L2 > R1)
-                hi = mid2 - 1;
+                hi = c2 - 1; // 减小c1，增大c2
             else
                 return (Math.max(L1, L2) + Math.min(R1, R2)) / 2;
         }
